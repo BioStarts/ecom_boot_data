46f0fd3d4d686023df32e52d7380787ee2725d7b
BioStarts <ipyo.ipa@gmail.com>
2019-12-02 23:04:06 +0300

    -added pagination and main page

--- src/main/java/com/geekbrains/bootdata/repositories/ItemRepository.java
+++ src/main/java/com/geekbrains/bootdata/repositories/ItemRepository.java
@@ -4,6 +4,8 @@ import com.geekbrains.bootdata.entities.Item;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
+import javax.rmi.CORBA.UtilDelegate;
+
 public interface ItemRepository extends JpaRepository<Item, Long>, JpaSpecificationExecutor<Item> {
 
 }
